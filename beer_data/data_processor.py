bf6ae876b407d15dfdc1cb5df66289a2b22595dd
Paulius Mikalajunas <psypm1@nottingham.ac.uk>
2020-02-28 23:50:31 +0800

    Basic UI with Google Maps

--- beer_data/data_processor.py
+++ beer_data/data_processor.py
@@ -2,15 +2,21 @@
 import numpy as np
 import timeit
 
-
-MAX_DISTANCE = 2000
-
 from .models import Brewery
 
+# Max distance that helicopter can travel.
+MAX_DISTANCE = 2000
+# Id of a home (starting) node.
+HOME_NODE = -2
 # Earth radius (in KM)
 EARTH_RADIUS = 6367
 
 def haversine(lat1, lon1, lat2, lon2):
+
+    # If we are dealing with the same node, we want infinite distance.
+    if lat1 == lat2 and lon1 == lon2:
+        return float('inf')
+
     lon1, lat1, lon2, lat2 = map(np.radians, [lon1, lat1, lon2, lat2])
 
     dlon = lon2 - lon1
@@ -22,24 +28,57 @@ def haversine(lat1, lon1, lat2, lon2):
     km = EARTH_RADIUS * c
     return km
 
-def construct_distance_matrix(lat, long):
-    start = timeit.default_timer()
-    # Pick out breweries that are less than (MAX_DISTANCE / 2)
-    matrix = []
-    # Calculate distance from current node to every brewery.
-    matrix.append(construct_distance_row(lat, long))
+
+def create_nodes(home_lat, home_long):
+    nodes = [Node(HOME_NODE, home_lat, home_long)]
     for brewery in Brewery.objects.all():
-        matrix.append(construct_distance_row(lat, long))
+        distance = haversine(home_lat, home_long, brewery.latitude, brewery.longitude)
+        # If distance is more than it will take us to go forth and back, we'll ignore the brewery.
+        if distance > (MAX_DISTANCE / 2):
+            continue
+        nodes.append(Node(brewery.id, brewery.latitude, brewery.longitude))
+    return nodes
+
+
+def construct_distance_matrix(nodes):
+    start = timeit.default_timer()
+    matrix = []
+    for node in nodes:
+        matrix.append(construct_distance_row(node, nodes))
 
     stop = timeit.default_timer()
     time = stop - start;
     return (matrix, time)
         
 
-def construct_distance_row(lat, long):
+def construct_distance_row(current_node, nodes):
     row = []
-    for brewery in Brewery.objects.all():
-        row.append(haversine(lat, long, brewery.latitude, brewery.longitude))
+    for node in nodes:
+        row.append(
+            haversine(current_node.lat, current_node.long, node.lat, node.long)
+        )
     return row
 
 
+def brute_force(matrix, nodes):
+    # Adding the home node as the first move.
+    solution_vector = [nodes[0]]
+    for row in matrix:
+        min, index = find_min(row)
+
+
+def find_min(row):
+    min = float('inf')
+    index = 0
+    for i in range(len(row)):
+        if row[i] < min:
+            min = row[i]
+            index = i
+    return (min, index)
+
+class Node:
+    def __init__(self, id, lat, long):
+        self.id = id
+        self.lat = lat
+        self.long = long
+        self.visited = False
--- beer_test/views.py
+++ beer_test/views.py
@@ -3,13 +3,24 @@ from django.http import HttpResponse
 
 from beer_data.csv_reader import *
 from beer_data.data_processor import *
+from beer_data.forms import SubmitForm
 
+# 51.355468, 11.100790
 def home_view(request):
-
     read_csv_data()
-    matrix, matrix_time = construct_distance_matrix(51.355468, 11.100790)
-    print('It took %dms to construct distance matrix' % matrix_time)
+
+    submit_form = SubmitForm()
+    if request.method == 'POST':
+        submit_form = SubmitForm(request.POST)
+        if submit_form.is_valid():
+            nodes = create_nodes(
+                submit_form.cleaned_data.get('lat'),
+                submit_form.cleaned_data.get('long')
+            )
+            matrix, matrix_time = construct_distance_matrix(nodes)
+            print('It took %dms to construct distance matrix' % matrix_time)
+            print(len(nodes))
     data = {
-        
+        "form": submit_form
     }
     return render(request, 'home.html', data)
